014066b02fbcfb507ff09fa3ef542685a1d4e4af
victorurd92 <vdurdinola_usistemas@intep.edu.co>
2025-09-01 19:40:55 -0500

    Actualizacion en script.js

--- public/script.js
+++ public/script.js
@@ -24,11 +24,11 @@ document.getElementById("btnBorrar").addEventListener("click", () => {
   document.getElementById("resultado").textContent = "";
 });
 
-document.getElementById("btnCampus").addEventListener("click", () => {
-  window.open("https://campus.intep.edu.co", "_blank");
-});
-
 document.getElementById("btnVertodos").addEventListener("click", () => {
   window.open("https://aws.amazon.com/es/what-is/api/", "_blank");
+});
+
+document.getElementById("btnCampus").addEventListener("click", () => {
+  window.open("https://campus.intep.edu.co", "_blank");
 });
 
