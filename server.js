243060a05445e497cb195ac1411816ff3e64ab8f
victorurd92 <vdurdinola_usistemas@intep.edu.co>
2025-09-07 15:52:25 -0500

    Fix server.js para Render: ruta catch-all y mejor compatibilidad

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Archivos estáticos (HTML, CSS, imágenes, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+// Render maneja todas las rutas desconocidas devolviendo index.html
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Arrancar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
-
